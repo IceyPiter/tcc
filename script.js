baeaf19656a0666a735d7b0448040b076eb34f7e
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-09-17 21:37:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function color() {
+    const texto = document.getElementById('texto-teste');
+    texto.style.color = "red"
+};
+function color2() {
+    const texto = document.getElementById('texto-teste');
+    texto.style.color = "white"
+};
