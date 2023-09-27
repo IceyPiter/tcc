7eb23f7387fd8e892a2d6f68f4d28fd6347d5f11
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-09-27 18:06:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function color() {
-    const texto = document.getElementById('texto-teste');
+function color(id) {
+    const texto = document.getElementById(id);
     texto.style.color = "red"
 };
-function color2() {
-    const texto = document.getElementById('texto-teste');
+function color2(id) {
+    const texto = document.getElementById(id);
     texto.style.color = "white"
 };
