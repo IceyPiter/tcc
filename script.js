a822a199a5db1306678977c5fccfda98cfc534c5
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-09-29 13:57:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function color(id) {
-    const texto = document.getElementById(id);
-    texto.style.color = "red"
-};
-function color2(id) {
-    const texto = document.getElementById(id);
-    texto.style.color = "white"
-};
+function login_registrar(){
+    document.getElementById("Login").innerHTML = "Login";
+    document.getElementById("Registrar").innerHTML = "Registrar-se";
+}
