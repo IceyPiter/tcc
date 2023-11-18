b1fce4b012ded064504089827c551f712fec7ca2
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-11-18 10:41:13 -0300

    Update models.py

--- Tcc/Entrada/models.py
+++ Tcc/Entrada/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Mensage(models.Model):
     id = models.DecimalField(primary_key=True, max_digits=20, decimal_places=0)
-    mensage = models.CharField(max_length=400, blank=True)
+    mensage = models.CharField(max_length=2000, blank=True)
     id_user = models.CharField(max_length=150, blank=True)
     key = models.ForeignKey("self", related_name="self", on_delete=models.CASCADE, null=True, blank=True)
 
